d5697c3367c735073b5c5d487d9a7a9803d4bc40
Abiala <59641440+Abiala@users.noreply.github.com>
2020-07-05 13:40:20 +0100

    modified

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
-f = open('Data/portfolio.csv', 'rt')
-header = next(f).split()
-total_shares = 0
-for line in f:
-    line = line.replace('\n', '')
-    row = line.split(',')
-    print(row)
-    total = int(row[1]) * float(row[2])
-    total_shares = total_shares + total
-       
-print('Total number of shares: '+ str(total_shares))
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    header = next(f).split()
+    total_shares = 0
+    for rowno, row in enumerate(f, start=1):
+        try:
+            row = row.replace('\n', '')
+            row = row.split(',')
+            print(row)
+            total = int(row[1]) * float(row[2])
+            total_shares = total_shares + total
+        except ValueError:
+            print(f'Row {rowno}: Bad row: {row}')
+
+    print('Total number of shares: '+ str(total_shares))
+portfolio_cost('Data/missing.csv')
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,47 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+def read_portfolio(filename):
+    '''Read a stock portfolio file into a list of dictionaries
+       with keys names, shares and prices. 
+    '''
+    portfolio = []
+    with open(filename) as f:
+        k = csv.reader(f)
+        next(k)
+        for line in f:
+            row = line.split(',')
+            stock = {'names': row[0] , 'shares': int(row[1]), 'price': float(row[2])}
+            portfolio.append(stock)
+    print(portfolio)
+portfolio = read_portfolio('Data/portfolio.csv')   
+
+def read_prices(filename):
+    '''Read a price csv file and store the data with names as key
+    '''
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+        print(prices)
+prices = read_prices('Data/prices.csv')
+
+def make_report(portfolio, prices):
+    '''Make a list of tuples(names, shares, price, change) given a portfolio list
+       and price dictionary
+    '''
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['names']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
+
+
+    
