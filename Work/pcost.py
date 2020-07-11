f94fde95ecbce84c2e5b6b1e13bc73d273154cf3
Abiala <59641440+Abiala@users.noreply.github.com>
2020-07-11 22:53:00 +0100

    modified

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,17 +3,18 @@
 # Exercise 1.27
 def portfolio_cost(filename):
     f = open(filename, 'rt')
-    header = next(f).split()
-    total_shares = 0
-    for rowno, row in enumerate(f, start=1):
+    header = next(f)
+    print(header)
+    rows = next(f)
+    total_cost = 0
+    for rowno, row in enumerate(rows, start=1):
+        record = dict(zip(header, rows))
         try:
-            row = row.replace('\n', '')
-            row = row.split(',')
-            print(row)
-            total = int(row[1]) * float(row[2])
-            total_shares = total_shares + total
+           nshares = int(record['shares'])
+           price = float(record['price'])
+           total_cost += nshares * price
         except ValueError:
             print(f'Row {rowno}: Bad row: {row}')
 
-    print('Total number of shares: '+ str(total_shares))
-portfolio_cost('Data/missing.csv')
+    print('Total number of shares: '+ str(total_cost))
+portfolio_cost('Data/portfoliodate.csv')
--- Work/report.py
+++ Work/report.py
@@ -8,14 +8,14 @@ def read_portfolio(filename):
     '''
     portfolio = []
     with open(filename) as f:
-        k = csv.reader(f)
-        next(k)
-        for line in f:
-            row = line.split(',')
-            stock = {'names': row[0] , 'shares': int(row[1]), 'price': float(row[2])}
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            record = dict(zip(header, row))
+            stock = {'name': record['name'] , 'shares': int(record['shares']), 'price': float(record['price'])}
             portfolio.append(stock)
-    print(portfolio)
-portfolio = read_portfolio('Data/portfolio.csv')   
+    return(portfolio)
+   
 
 def read_prices(filename):
     '''Read a price csv file and store the data with names as key
@@ -28,20 +28,44 @@ def read_prices(filename):
                 prices[row[0]] = float(row[1])
             except IndexError:
                 pass
-        print(prices)
-prices = read_prices('Data/prices.csv')
+        return(prices)
 
-def make_report(portfolio, prices):
+
+def  make_report(portfolio, prices):
     '''Make a list of tuples(names, shares, price, change) given a portfolio list
        and price dictionary
     '''
     rows = []
     for stock in portfolio:
-        current_price = prices[stock['names']]
+        current_price = prices[stock['name']]
         change = current_price - stock['price']
         summary = (stock['name'], stock['shares'], current_price, change)
         rows.append(summary)
     return rows
 
+def print_report(report):
+    '''
+    Print a formatted table from a list of (names, shares, price, change) tuples.
+    '''
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s %10s %10s %10s' % headers)
+    print(('-' * 10 + ' ') * len(headers))
+    for row in report:
+        print('%10s %10d %10.2f %10.2f' % row)
 
+def portfolio_report(portfoliofile, pricefile):
+    '''
+    Read the portfolio and price csv files.
+    '''
+    portfolio = read_portfolio(portfoliofile)
+    prices = read_prices(pricefile)
+
+    #Make a report.
+
+    report = make_report(portfolio, prices)
     
+    #Output the report
+
+    print_report(report)
+
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
